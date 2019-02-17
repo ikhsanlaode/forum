9c6da8787fb0095f9442bd1a2b118f56caf3b575
ikhsanlaode <pb.ichsan30@gmail.com>
2019-02-17 11:14:34 +0700

    continue develop welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
         <meta charset="utf-8">  
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -29,19 +30,41 @@
                 
             </div>
         </nav>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        <div class="dropdown">
+            <button class="btn btn-info  btn-sm dropdown-toggle" type="button" id="xx" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Category
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PC Games</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Online Games</a>
+                </li>
+            </ul>
+        </div>
+        </nav>
         <div class="modal" id="login">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-body">
                     <form>
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <label for="email-login">Email address</label>
+                            <input type="email" class="form-control" id="email_login" aria-describedby="emailHelp" placeholder="Enter email">
                             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            <label for="password-login">Password</label>
+                            <input type="password" class="form-control" id="pass_login" placeholder="Password">
                         </div>
                         <div class="form-group form-check">
                             <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -60,24 +83,88 @@
                     <form>
                         <div class="form-group">
                             <label for="name">Nama</label>
-                            <input type="text" class="form-control" id="name_id"  placeholder="Masukkan nama lengkap">
+                            <input type="text" class="form-control" id="name_register"  placeholder="Masukkan nama lengkap" name ="name">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan email">
+                            <label for="email-register">Email</label>
+                            <input type="email" class="form-control" id="email_register" aria-describedby="emailHelp" placeholder="Masukkan email" name ="email">
                             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                         </div>
                         <div class="form-group">
                             <label for="user">Username</label>
-                            <input type="text" class="form-control" id="user_id"  placeholder="Masukkan Username">
+                            <input type="text" class="form-control" id="user_register"  placeholder="Masukkan Username" name ="username">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            <label for="password-register">Password</label>
+                            <input type="password" class="form-control" id="pass_register" placeholder="Password" name="password">
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                     </div>               
+                </div>
+            </div>
+        </div>
+        </br>
+        <div class="container">
+            <div class = "row">
+                <div class = "col-md-8">
+                    <table class="table table-bordered">
+                        <thead class="table-primary">
+                            <tr>
+                            <th style="width:50%">First Category</th>
+                            <th style="width:10%" align ="center">Topics</th>
+                            <th style="width:10%" align ="center">Post</th>
+                            <th style="width:30%" align ="center">Last post</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>First Sub category</td>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>by <a href="#">ikhsan <i class="fas fa-chevron-right"></i></a>
+                                <br>
+                                17-02-2019 9:12
+                            </td>
+                            </tr>
+                            <tr>
+                            <td>Second Sub category</td>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>by <a href="#">ikhsan <i class="fas fa-chevron-right"></i></a>
+                                <br>
+                                17-02-2019 9:12
+                            </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class = "col-md">
+                    <div class="card">
+                        <div class="card-header card text-white bg-info mb-3">
+                            NEW FORUM POSTS
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                Testing post
+                                <br>
+                                by <a href ="#"> ikhsan </a>
+                                <br>
+                                17-02-2019 10:19
+                            </li>
+                        </ul>
+                    </div>
+                    <br>
+                    <div class="card">
+                        <div class="card-header card text-white bg-info mb-3">
+                            NEW FORUM POSTS
+                        </div>
+                        <div class ="card-body">
+                            Total member: 9999 <br>
+                            Total post: 9999 <br>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
